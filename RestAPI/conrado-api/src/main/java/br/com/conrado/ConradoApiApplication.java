f506b17c912f33833a1b9ae31f31ab50fec216fc
João Conrado <99970376+JoaoSConrado@users.noreply.github.com>
2022-09-27 17:24:57 -0300

    Add files via upload

---
+++ RestAPI/conrado-api/src/main/java/br/com/conrado/ConradoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.conrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConradoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConradoApiApplication.class, args);
+	}
+
+}
---
+++ RestAPI/conrado-api/src/test/java/br/com/conrado/ConradoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConradoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
